2f9d7053ac98e3b87f2210ae631b39ab868aa814
minhquangmcv <minhquang.lien@mcvgo.com>
2022-02-23 10:26:25 +0700

    update

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 require ('dotenv').config()
 const telegram = require('node-telegram-bot-api')
+process.env.NTBA_FIX_319 = 1;
 
 try {
 
     const bot = new telegram(core.getInput('TELEGRAM_TOKEN'));
-    bot.sendMessage(core.getInput('TELEGRAM_TO'), 'Hello im git action');
+    bot.sendMessage(core.getInput('TELEGRAM_TO'), core.getInput('message'));
 
 } catch (error) {
   core.setFailed(error.message);
