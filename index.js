30f59d101637a573bb96dac415cc94ecec76cc5c
minhquangmcv <minhquang.lien@mcvgo.com>
2022-02-24 10:23:07 +0700

    ! add send file

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const telegram = require('node-telegram-bot-api')
 process.env.NTBA_FIX_319 = 1;
 
 try {
-
+    const chatID = core.getInput('TELEGRAM_TO');
     const bot = new telegram(core.getInput('TELEGRAM_TOKEN'));
-    bot.sendMessage(core.getInput('TELEGRAM_TO'), core.getInput('message'));
+    bot.sendMessage(chatID, core.getInput('message'));
+
+    const filePath = core.getInput('file');
+    if(filePath != "") {
+        await bot.sendAudio(chatID, filePath);
+    }
+    
 
 } catch (error) {
   core.setFailed(error.message);
